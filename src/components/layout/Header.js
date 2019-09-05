4dea040265bfd6537758f2770fa41adfade9926f
David Glines <dglines86@gmail.com>
2019-09-04 21:13:30 -0400

    pull tasks from firebase

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,22 +1,18 @@
 import React from "react"
 import { FaPizzaSlice } from "react-icons/fa" // TODO: look up more on font awesome
 
-export const Header = () => {
-  const temp = 1
-
-  return (
-    <header className="header" data-testid="header">
-      <nav>
-        <div className="logo"></div>
-        <div className="settings">
-          <ul>
-            <li>+</li>
-            <li>
-              <FaPizzaSlice />
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </header>
-  )
-}
+export const Header = () => (
+  <header className="header" data-testid="header">
+    <nav>
+      <div className="logo"></div>
+      <div className="settings">
+        <ul>
+          <li>+</li>
+          <li>
+            <FaPizzaSlice />
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+)
--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app"
 import "firebase/firestore"
 
 const firebaseConfig = firebase.initializeApp({
-  apiKey: "AIzaSyDLreXnoECBrP1qo48m8giaXVcTZEFqmV0",
-  authDomain: "todo-3cfe7.firebaseapp.com",
-  databaseURL: "https://todo-3cfe7.firebaseio.com",
-  projectId: "todo-3cfe7",
-  storageBucket: "todo-3cfe7.appspot.com",
-  messagingSenderId: "754770515304",
-  appId: "1:754770515304:web:69f5da4d4aa458fc"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 })
 
 export { firebaseConfig as firebase }
